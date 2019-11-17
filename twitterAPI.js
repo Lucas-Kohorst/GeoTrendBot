bac20ae4a3c0620f328fcdc77223af9e2aebbb25
Lucas Kohorst <kohorstlucas@gmail.com>
2019-11-17 13:57:12 -0500

    Updated Credentials

--- twitterAPI.js
+++ twitterAPI.js
@@ -1,12 +1,8 @@
 var Twitter = require("twitter");
 var scrapeTwitter = require("scrape-twitter");
+var config = require("./.config.js")
 
-var client = new Twitter({
-    consumer_key: "yGNZeX4d4hHRWkpnZ2TCBWdZ6",
-    consumer_secret: "asOAgcrcjVmArgRUltV2JcCLbRnPVWf6MKCQ6cSJbbqxxmXgWs",
-    access_token_key: "981264506214080512-aC58OZliG1jiOV1Kl4NZVdwl8tNeeVy",
-    access_token_secret: "o77pQQy5yU8Zfd004AfvSci5yX9wJfM3ra80O5EjjFY9h"
-});
+var client = new Twitter(config.config);
 
 /**
  * Get the mentions timeline for the authenticated user
