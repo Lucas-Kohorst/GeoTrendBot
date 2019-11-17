550412b7dc197bdb6cc3755c1d036c4fa3eb6561
Lucas Kohorst <kohorstlucas@gmail.com>
2019-11-17 14:06:15 -0500

    Added Creds to Heroku

---
+++ .config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    config: {
+        consumer_key: "yGNZeX4d4hHRWkpnZ2TCBWdZ6",
+        consumer_secret: "asOAgcrcjVmArgRUltV2JcCLbRnPVWf6MKCQ6cSJbbqxxmXgWs",
+        access_token_key: "981264506214080512-aC58OZliG1jiOV1Kl4NZVdwl8tNeeVy",
+        access_token_secret: "o77pQQy5yU8Zfd004AfvSci5yX9wJfM3ra80O5EjjFY9h"
+    }
+};
